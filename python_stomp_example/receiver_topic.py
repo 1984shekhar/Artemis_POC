6cbb656f1183426c414f962f5a4be8272a293f0b
Chandra Shekhar Pandey <cpandey@cpandey.pnq.csb>
2018-06-13 08:32:48 +0530

    durable topic artemis headers

--- python_stomp_example/receiver_topic.py
+++ python_stomp_example/receiver_topic.py
@@ -14,11 +14,11 @@ conn = stomp.Connection(host_and_ports=hosts)
 conn.set_listener('', MyListener())
 conn.start()
 conn.connect('admin', 'admin', wait=True,headers = {'client-id': 'clientname'} )
-conn.subscribe(destination='/topic/test', id=1, ack='auto',headers = {'activemq.subscriptionName': 'myhostname'})
+conn.subscribe(destination='A.B.C.D', id=1, ack='auto',headers = {'subscription-type': 'MULTICAST','durable-subscription-name':'someValue'})
 #conn.subscribe({destination=config['/topic/test'], ack:'auto', 'activemq.subscriptionName':'SampleSubscription'})
 #conn.subscribe(destination='/topic/testTopic', ack='auto', headers = {'activemq.subscriptionName': 'myhostname'})
 
-conn.send(body=' '.join(sys.argv[1:]), destination='/topic/test')
+conn.send(body=' '.join(sys.argv[1:]), destination='A.B.C.D')
 
 time.sleep(2)
 conn.disconnect()
