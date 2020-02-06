341f2eb0e6fa53d407ee9b7f844f750f37c28ab4
Chandra Shekhar Pandey <cpandey@cpandey.pnq.csb>
2020-02-06 21:27:53 +0530

    eap7-sender-receiver

---
+++ eap-sender-receiver/src/main/java/com/mypackage/QueueReceiveEAP.java
@@ -0,0 +1,101 @@
+package com.mypackage;
+
+import java.util.Hashtable;
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class QueueReceiveEAP implements MessageListener {
+
+	public final static String JNDI_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+	// *************** Connection Factory JNDI name *************************
+	public final static String JMS_FACTORY = "jms/RemoteConnectionFactory";
+	// *************** Queue JNDI name *************************
+	public final static String QUEUE = "jms/TestQueue";
+	
+	public final static String URL="remote://localhost:4447";
+	
+	private QueueConnectionFactory  qconFactory;
+	private QueueConnection qcon;
+	private QueueSession qsession;
+	private QueueReceiver qreceiver;
+	private Queue queue;
+	private boolean quit = false;
+	private String userName="jms";
+	private String password="jms@1234";
+	
+
+	public void onMessage(Message msg) {
+		try {
+			String msgText;
+			if (msg instanceof TextMessage) {
+				msgText = ((TextMessage) msg).getText();
+			} else {
+				msgText = msg.toString();
+			}
+			System.out.println("Msg_Receiver = " + msgText);
+			
+			qsession.commit();
+
+			if (msgText.equalsIgnoreCase("quit")) {
+				synchronized (this) {
+					quit = true;
+					qsession.commit();
+					this.notifyAll(); // Notify main thread to quit
+				}
+			}
+		} catch (JMSException jmse) {
+			jmse.printStackTrace();
+		}
+
+	}
+
+	public void init(Context ctx, String queueName) throws NamingException,
+			JMSException {
+		qconFactory = (QueueConnectionFactory ) ctx.lookup(JMS_FACTORY);
+		//qconFactory.getRedeliveryPolicy().setMaximumRedeliveries(2);
+		qcon = qconFactory.createQueueConnection(userName, password);
+		//qcon.setClientID("Hello Shailendra");
+		qsession = qcon.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
+
+		queue = (Queue) ctx.lookup(queueName);
+		qreceiver = qsession.createReceiver(queue);
+		qreceiver.setMessageListener(this);
+		qcon.start();
+	}
+
+	public void close() throws JMSException {
+		qreceiver.close();
+		qsession.close();
+		qcon.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext ic = getInitialContext(URL);
+		QueueReceiveEAP qr = new QueueReceiveEAP();
+		qr.init(ic, QUEUE);
+		System.out
+				.println("JMS Ready To Receive Messages (To quit, send a  message from QueueSender.class).");
+		// Wait until a "quit" message has been received.
+		synchronized (qr) {
+			while (!qr.quit) {
+				try {
+					qr.wait();
+				} catch (InterruptedException ie) {
+				}
+			}
+		}
+		qr.close();
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private static InitialContext getInitialContext(String url)
+			throws NamingException {
+		Hashtable env = new Hashtable();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
+		env.put(Context.PROVIDER_URL, url);
+		return new InitialContext(env);
+	}
+}
---
+++ eap-sender-receiver/src/main/java/com/mypackage/QueueSendEAP.java
@@ -0,0 +1,97 @@
+package com.mypackage;
+
+import java.io.*;
+import java.util.Hashtable;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class QueueSendEAP {
+	public final static String JNDI_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+	// *************** Connection Factory JNDI name *************************
+	public final static String JMS_FACTORY = "jms/RemoteConnectionFactory";
+	// *************** Queue JNDI name *************************
+	public final static String QUEUE = "jms/queue/InQueue";
+	
+	public final static String URL="http-remoting://localhost:8080";
+
+	private QueueConnectionFactory qconFactory;
+	private QueueConnection qcon;
+	private QueueSession qsession;
+	private QueueSender qsender;
+	private Queue queue;
+	private TextMessage msg;
+	private String userName="admin";
+	private String password="admin";
+	
+
+	public void init(Context ctx, String queueName) throws NamingException,
+			JMSException {
+		qconFactory = (QueueConnectionFactory) ctx.lookup(JMS_FACTORY);
+		//qcon = qconFactory.createQueueConnection();
+		qcon = qconFactory.createQueueConnection(userName, password);
+		qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		queue = (Queue) ctx.lookup(queueName);
+		qsender = qsession.createSender(queue);
+//		qsender.setTimeToLive(1000);
+		msg = qsession.createTextMessage();
+		qcon.start();
+	}
+
+	public void send(String message) throws JMSException {
+		msg.setText(message);
+		//for(int i=0;i<=100000;i++)
+		qsender.send(msg);
+	}
+
+	public void close() throws JMSException {
+		qsender.close();
+		qsession.close();
+		qcon.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+		InitialContext ic = getInitialContext(URL);
+		QueueSendEAP qs = new QueueSendEAP();
+		qs.init(ic, QUEUE);
+		readAndSend(qs);
+		qs.close();
+	}
+
+	private static void readAndSend(QueueSendEAP qs) throws IOException,
+			JMSException {
+		String line = "Test Message Body with counter s = ";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean readFlag = true;
+		System.out.println("ntStart Sending Messages (Enter QUIT to Stop):n");
+		while (readFlag) {
+			System.out.print("<Msg_Sender> ");
+			String msg = br.readLine();
+			if (msg.equals("QUIT") || msg.equals("quit")) {
+				qs.send(msg);
+				System.exit(0);
+			}
+			qs.send(msg);
+			System.out.println();
+		}
+		br.close();
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private static InitialContext getInitialContext(String url)
+			throws NamingException {
+		Hashtable env = new Hashtable();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
+		env.put(Context.PROVIDER_URL, url);
+		return new InitialContext(env);
+	}
+}
