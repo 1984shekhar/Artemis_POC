769fc1d76bf6baf38687e1ca2f663b0adaa98a6e
Chandra Shekhar <chandrashekhar@localhost.localdomain>
2020-03-22 00:56:10 +0530

    qpid proton cpp clients

---
+++ amqclients_cpp/AsyncSend/include/simple_send.cpp.h
@@ -0,0 +1,16 @@
+#ifndef SIMPLE_SEND_CPP_H
+#define SIMPLE_SEND_CPP_H
+
+
+class simple_send.cpp
+{
+    public:
+        simple_send.cpp();
+        virtual ~simple_send.cpp();
+
+    protected:
+
+    private:
+};
+
+#endif // SIMPLE_SEND_CPP_H
