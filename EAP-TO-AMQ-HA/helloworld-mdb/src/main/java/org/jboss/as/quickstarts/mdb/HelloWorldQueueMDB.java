a7b5f5a365fa42bf88cf981d0615a9d5abb4fe64
Chandra Shekhar <chandrashekhar@localhost.localdomain>
2021-03-05 22:50:16 +0530

    EAP-TO-AMQ-HA/helloworld-mdb

---
+++ EAP-TO-AMQ-HA/helloworld-mdb/src/main/java/org/jboss/as/quickstarts/mdb/HelloWorldQueueMDB.java
@@ -0,0 +1,46 @@
+package org.jboss.as.quickstarts.mdb;
+
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.jboss.ejb3.annotation.ResourceAdapter;
+
+/**
+ * <p>
+ * A simple Message Driven Bean that asynchronously receives and processes the messages that are sent to the queue.
+ * </p>
+ *
+ * @author Serge Pagop (spagop@redhat.com)
+ */
+@MessageDriven(name = "HelloWorldQueueMDB", activationConfig = {
+@ActivationConfigProperty(propertyName = "useJNDI", propertyValue = "false"),
+@ActivationConfigProperty(propertyName = "destination", propertyValue = "MyQueue"),
+     })
+@ResourceAdapter("activemq-ra-remote")
+ public class HelloWorldQueueMDB implements MessageListener {
+
+    private static final Logger LOGGER = Logger.getLogger(HelloWorldQueueMDB.class.toString());
+
+    /**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message rcvMessage) {
+        TextMessage msg = null;
+        try {
+            if (rcvMessage instanceof TextMessage) {
+                msg = (TextMessage) rcvMessage;
+                LOGGER.info("Received Message from queue: " + msg.getText());
+            } else {
+                LOGGER.warning("Message of wrong type: " + rcvMessage.getClass().getName());
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ EAP-TO-AMQ-HA/helloworld-mdb/src/main/java/org/jboss/as/quickstarts/servlet/HelloWorldMDBServletClient.java
@@ -0,0 +1,87 @@
+package org.jboss.as.quickstarts.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSDestinationDefinitions;
+import javax.jms.Queue;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Definition of the two JMS destinations used by the quickstart
+ * (one queue and one topic).
+ */
+@JMSDestinationDefinitions(
+    value = {
+        @JMSDestinationDefinition(
+            name = "java:/queue/MyQueue",
+            interfaceName = "javax.jms.Queue",
+            destinationName = "MyQueue"
+        )
+    }
+)
+
+/**
+ * <p>
+ * A simple servlet 3 as client that sends several messages to a queue or a topic.
+ * </p>
+ *
+ * <p>
+ * The servlet is registered and mapped to /HelloWorldMDBServletClient using the {@linkplain WebServlet
+ * @HttpServlet}.
+ * </p>
+ *
+ * @author Serge Pagop (spagop@redhat.com)
+ *
+ */
+@WebServlet("/HelloWorldMDBServletClient")
+public class HelloWorldMDBServletClient extends HttpServlet {
+
+    private static final long serialVersionUID = -8314035702649252239L;
+
+    private static final int MSG_COUNT = 5;
+
+    @Inject
+    @JMSConnectionFactory("java:jboss/RemoteJmsXA")
+    private JMSContext context;
+
+    @Resource(lookup = "java:/MyQueue")
+    private Queue queue;
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("<h1>Quickstart: Example demonstrates the use of <strong>JMS 2.0</strong> and <strong>EJB 3.2 Message-Driven Bean</strong> in JBoss EAP.</h1>");
+        try {
+            final Destination destination =  queue;
+            out.write("<p>Sending messages to <em>" + destination + "</em></p>");
+            out.write("<h2>The following messages will be sent to the destination:</h2>");
+            for (int i = 0; i < MSG_COUNT; i++) {
+                String text = "This is message " + (i + 1);
+                context.createProducer().send(destination, text);
+                out.write("Message (" + i + "): " + text + "</br>");
+            }
+            out.write("<p><i>Go to your JBoss EAP server console or server log to see the result of messages processing.</i></p>");
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
